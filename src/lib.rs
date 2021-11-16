afad117684225995c28a5fb15dac814e387a7fff
Bram Neijt <bram@neijt.nl>
2021-11-16 22:47:36 +0100

    Add one_off

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,30 @@
+
+extern crate tera;
+use pyo3::exceptions;
+use tera::Context;
+use tera::Tera;
+use std::collections::HashMap;
 use pyo3::prelude::*;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn one_off(template: String, context_dict: HashMap<String, String>, autoescape: bool) -> PyResult<String> {
+    let mut context = Context::new();
+    for (key, value) in context_dict {
+        context.insert(key, &value);
+    }
+    return match Tera::one_off(&template, &context, autoescape) {
+        Ok(t) => Ok(t),
+        Err(e) => Err(exceptions::PyValueError::new_err(e.to_string())),
+    }
 }
 
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
-fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+fn pytera(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(one_off, m)?)?;
 
     Ok(())
 }
